fa72f4caf96fd9f4377393beb8c4173300776d61
azizaweb <azizaweb@mail.ru>
2025-02-21 21:59:21 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function imgSlider(anything){
+    document.querySelector('.peach').src = anything;
+} 
+function changeBgColor(color){
+    const sec = document.querySelector('.sec');
+    sec.style.background = color;
+}
